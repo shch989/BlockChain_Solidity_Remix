ccf19af640fb00ff3ad4339824b506166d224cb5
shch989 <tankpop11@gmail.com>
2023-08-28 22:55:40 +0900

    Booleans

---
+++ ExampleBoolean.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract ExampleBoolean {
+    bool public myBool;
+
+    function setMyBool(bool _MyBool) public {
+        myBool = _MyBool;
+    }
+}
