4f47132d22e8e3fbb85723308700f79a63836c4c
shch989 <tankpop11@gmail.com>
2023-08-29 22:38:47 +0900

    msg

---
+++ ExampleMegSender.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract ExampleMegSender {
+    address public someAddress;
+
+    function updateSomeAddress() public {
+        someAddress = msg.sender;
+    }
+
+}
